5f70a2c64d75ebe82b29558a3cc25a6b1a570596
Tharusan Vijayakumar <tharusan.vijayakumar@gmail.com>
2023-03-13 11:26:32 +0000

    Assets added

--- src/application/Main.java
+++ src/application/Main.java
@@ -21,63 +21,69 @@ import javafx.scene.Parent;
  * Nodes (Different components added to a scene e.g. button, images, text boxes)
  */
 public class Main extends Application {
-        public void start(Stage boardStage) {
-            try {
-                GridPane grid = new GridPane();
-                grid.setPrefSize(800, 800);
 
-                // Create the brown and white checked pattern
-                boolean isWhite = true;
-                for (int row = 0; row < 8; row++) {
-                    for (int col = 0; col < 8; col++) {
-                        Rectangle square = new Rectangle(100, 100);
-                        if (isWhite) {
-                            square.setFill(Color.WHITE);
-                        } else {
-                            square.setFill(Color.BROWN);
-                        }
-                        grid.add(square, col, row);
-                        isWhite = !isWhite;
+    //Piece Images
+    String w_pawnImageString = "assets/white_pawn.png";
+    String b_pawnImageString = "assets/black_pawn.png";
+    String w_kingImageString = "assets/white_king.png";
+    String b_kingImageString = "assets/black_king.png";
+    String w_rookImageString = "assets/white_rook.png";
+    String b_rookImageString = "assets/black_rook.png";
+    String w_knightImageString = "assets/white_knight.png";
+    String b_knightImageString = "assets/black_knight.png";
+    String w_bishopImageString = "assets/white_bishop.png";
+    String b_bishopImageString = "assets/black_bishop.png";
+    String w_queenImageString = "assets/white_queen.png";
+    String b_queenImageString = "assets/black_queen.png";
+
+    public void start(Stage boardStage) {
+        try {
+            GridPane grid = new GridPane();
+            grid.setPrefSize(800, 800);
+
+            // Create the brown and white checked pattern
+            boolean isWhite = true;
+            for (int row = 0; row < 8; row++) {
+                for (int col = 0; col < 8; col++) {
+                    Rectangle square = new Rectangle(100, 100);
+                    if (isWhite) {
+                        square.setFill(Color.WHITE);
+                    } else {
+                        square.setFill(Color.BROWN);
                     }
+                    grid.add(square, col, row);
                     isWhite = !isWhite;
                 }
-
-                // Add buttons to the grid
-                for (int row = 0; row < 8; row++) {
-                    for (int col = 0; col < 8; col++) {
-                        Button button = new Button();
-                        button.setPrefSize(100, 100);
-                        button.setStyle("-fx-background-color: transparent;");
-                        grid.add(button, col, row);
-                    }
-                }
-
-                // Set the alignment of the grid to center
-                grid.setAlignment(Pos.CENTER);
-
-                //Board stuff
-                boardStage.setTitle("Chess");
-                Image icon = new Image("icon.png");
-                boardStage.getIcons().add(icon);
-                boardStage.setResizable(false);
-
-                //Setting the grid to the scene
-                Scene scene = new Scene(grid);
-                //Adding the scene to the stage
-                boardStage.setScene(scene);
-                //To see the stage
-                boardStage.show();
-
-            } catch(Exception e) {
-                e.printStackTrace();
+                isWhite = !isWhite;
             }
-        }
 
-        public static void main(String[] args) {
-            /*Static Method that belongs to the application class taking
-             * and when called we send our string array of arguments to the launch
-             * method. Behind the scenes the start method would be called*/
-            launch(args);
+            // Set the alignment of the grid to center
+            grid.setAlignment(Pos.CENTER);
+
+            //Board stuff
+            boardStage.setTitle("Chess");
+            Image icon = new Image("icon.png");
+            boardStage.getIcons().add(icon);
+            boardStage.setResizable(false);
+
+            //Setting the grid to the scene
+            Scene scene = new Scene(grid);
+            //Adding the scene to the stage
+            boardStage.setScene(scene);
+            //To see the stage
+            boardStage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
         }
     }
+
+    public static void main(String[] args) {
+        /*Static Method that belongs to the application class taking
+         * and when called we send our string array of arguments to the launch
+         * method. Behind the scenes the start method would be called*/
+        launch(args);
+    }
+
+}
 
